99fe6bac3dac2cf3e073543598c926c1cd8532df
e-last <statw54@gmail.com>
2021-02-25 12:16:29 +0000

    反映自動化

--- config/routes.rb
+++ config/routes.rb
@@ -50,10 +50,6 @@ Rails.application.routes.draw do
     end
 
     get '/search' => 'search#search'
-    
-    get   'inquiry'         => 'inquiry#index'     # 入力画面
-    post  'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
-    post  'inquiry/thanks'  => 'inquiry#thanks'    # 送信完了画面
 
     end
 
